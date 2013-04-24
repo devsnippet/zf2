c843356125e2e114a8de37134a02c9012aca2ca3
AShvager <AShvager@ddovnar.TSU>
2013-04-24 16:22:59 +0300

    Begin

--- Checkuser/Module.php
+++ Checkuser/Module.php
@@ -1,26 +1,11 @@
 <?php
 
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace Checkuser;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\Mvc\ModuleRouteListener;
-use Zend\ModuleManager\ModuleManagerInterface;
-use Zend\Authentication\AuthenticationService;
-//use Zend\Authentication\Adapter\AdapterInterface;
-use User\Services\Authentication as AuthServ;
 use Zend\Console\Adapter\AdapterInterface as Console;
-use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
-use Zend\ModuleManager\Feature\ConsoleBannerProviderInterface;
 
-class Module  implements AutoloaderProviderInterface{
+class Module implements AutoloaderProviderInterface {
 
  public function getAutoloaderConfig() {
   return array(
@@ -29,58 +14,9 @@ class Module  implements AutoloaderProviderInterface{
   );
  }
 
- /**
-  * Init function
-  *
-  * @return void
-  */
-//    public function init()    {
- //// Attach Event to EventManager
- //$events = StaticEventManager::getInstance();
- //$events->attach('Zend\Mvc\Controller\ActionController', 'dispatch', array($this, 'mvcPreDispatch'), 100); //@todo - Go directly to User\Event\Authentication
- //$eventManager = $manager->getEventManager();
- //$eventManager->attach(ModuleEvent::EVENT_LOAD_MODULES_POST,array($this, 'mvcPreDispatch'), -1100 );
-//    }
-
- public function init(ModuleManagerInterface $moduleManager) {
-  $sharedManager = $moduleManager->getEventManager()->getSharedManager();
-  $sharedManager->attach('Zend\Mvc\Application', 'dispatch', array($this, 'mvcPreDispatch'), 100);
- }
-
- public function mvcPreDispatch($event) {
-  $di = $event->getTarget()->getServiceManager();
-  $auth = $di->get('User\Event\Authentication');
-  return $auth->preDispatch($event);
- }
-
  public function getConfig() {
   return include __DIR__ . '/config/module.config.php';
  }
-
- public function onBootstrap($e) {
-  // You may not need to do this if you're doing it elsewhere in your
-  // application
-  //$eventManager        = $e->getApplication()->getEventManager();
-  //$moduleRouteListener = new ModuleRouteListener();
-  //$moduleRouteListener->attach($eventManager);
-  //$app    = $e->getTarget();
-  //$events = $app->getEventManager();
-  //$events->attach('dispatch', array($this, 'mvcPreDispatch'), -100);
- }
-
- /**
-  * MVC preDispatch Event
-  *
-  * @param $event
-  * @return mixed
-  */
- /*    public function mvcPreDispatch($event) {
-   $di = $event->getTarget()->getLocator();
-   $auth = $di->get('User\Event\Authentication');
-
-   return $auth->preDispatch($event);
-   }
-  */
 
  public function getViewHelperConfig() {
   return array(
@@ -92,38 +28,7 @@ class Module  implements AutoloaderProviderInterface{
 
  public function getServiceConfig() {
   return array(
-    /*       'initializers' => array(
-      function ($instance, $sm) {
-      if ($instance instanceof \Zend\Db\Adapter\AdapterAwareInterface) {
-      //$instance->setDbAdapter($sm->get('ZDBA'));
-
-      }
-      }
-      ), */
-    'factories' => array(
-      // 'BlankDB'      => function($sm) { return $bdb= new Model\BlankDB(); },
-      // 'ModelUtils'   => function($sm) { return $mu = new Model\ModelUtils();},
-      /*          'MClients'    => function($sm) {
-        $mc=new Model\ModelClients();
-        $mc->setDbAdapter($sm->get('ZDBA'));
-        return $mc;
-        } */
-      'AuthService' => function($sm) {
-       //$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
-       //$dbTableAuthAdapter = new DbTableAuthAdapter($dbAdapter, 'users', 'user_name', 'pass_word', 'MD5(?)');
-       //$authService = new AuthenticationService();
-       //$authService = new AuthenticationService();
-       //$authService->setAdapter($dbTableAuthAdapter);
-       //$authService->setAdapter($sm->get('ZDBA'));
-       $authService = new AuthServ();
-       //$authService->setAdapter(auth);
-       //$authService->setStorage($sm->get('SanAuth\Model\MyAuthStorage'));
-
-       return $authService;
-      },
-    ),
     'invokables' => array(
-    //   'Application\Model\ModelNews'=>'ModelNews'
     )
   );
  }
@@ -135,8 +40,8 @@ class Module  implements AutoloaderProviderInterface{
  public function getConsoleUsage(Console $console) {
   //description command
   return array(
-    'user --vesion' => 'Get current version',
-    'user -d' => 'debug session',
+    'checkuser --vesion' => 'Get current version',
+    'checkuser -d' => 'debug session',
   );
  }
 

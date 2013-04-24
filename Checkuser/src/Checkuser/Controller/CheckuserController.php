d3a91710edeac93a025463ab770676d7a8220cd8
AShvager <AShvager@ddovnar.TSU>
2013-04-24 17:56:34 +0300

    Begin

--- Checkuser/src/Checkuser/Controller/CheckuserController.php
+++ Checkuser/src/Checkuser/Controller/CheckuserController.php
@@ -35,7 +35,8 @@ use Checkuser\Form\Checkuser as CheckuserForm;
  * @license   http://binware.org/license/index/type:new-bsd New BSD License
  */
 class CheckuserController extends AbstractActionController {
-private $form;
+
+ private $form;
 
  public function getAuthService() {
   if (!$this->authservice) {
@@ -49,12 +50,18 @@ private $form;
   * Index Action
   */
  public function checkuserAction() {
-        $form       = $this->getForm();
-        return array(
-            'form'      => $form,
-            'messages'  => $this->flashmessenger()->getMessages()
-        );
+  $form = $this->getForm();
+  $request=$this->getRequest();
 
+  if ($request->isPost() && $form->isValid($request->getPost()->toArray())){
+   $messages='ssssssssssss';
+  }
+
+
+  return array(
+    'form' => $form,
+    'messages' => $this->flashmessenger()->getMessages()
+  );
  }
 
  public function getForm() {
@@ -73,14 +80,14 @@ private $form;
   * @return array
   */
  /*
- public function loginAction() {
-  //$form = new LoginForm();
-  $form = $this->getForm();
-  $request = $this->getRequest();
+   public function loginAction() {
+   //$form = new LoginForm();
+   $form = $this->getForm();
+   $request = $this->getRequest();
 
-  if ($request->isPost()
-  //&& $form->isValid($request->getPost()->toArray())
-  ) {
+   if ($request->isPost()
+   //&& $form->isValid($request->getPost()->toArray())
+   ) {
    //$uAuth = $this->getLocator()->get('User\Controller\Plugin\UserAuthentication'); //@todo - We must use PluginLoader $this->userAuthentication()!!
    $uAuth = $this->getServiceLocator()->get('User\Controller\Plugin\UserAuthentication');
    $authAdapter = $uAuth->getAuthAdapter();
@@ -89,72 +96,30 @@ private $form;
    $authAdapter->setCredential($form->getValue('password'));
 
    \Zend\Debug::dump($uAuth->getAuthService()->authenticate($authAdapter));
+   }
+
+   return array('loginForm' => $form);
+   }
+  */
+
+ public function updateAction() {
+  $request = $this->getRequest();
+  if ($request->isPost()) {
+   $form->setData($request->getPost());
+   if ($form->isValid()) {
+
+   }
   }
 
-  return array('loginForm' => $form);
+
+  $form = $this->getForm();
+
+  return array(
+    'form' => $form,
+    'messages' => $this->flashmessenger()->getMessages()
+  );
  }
-*/
-
-     public function updateAction()
-    {
-        //if already login, redirect to success page
-        if ($this->getAuthService()->hasIdentity()){
-            return $this->redirect()->toRoute('success');
-        }
-
-        $form       = $this->getForm();
-
-        return array(
-            'form'      => $form,
-            'messages'  => $this->flashmessenger()->getMessages()
-        );
-    }
-
-    public function authenticateAction()
-    {
-        $form       = $this->getForm();
-        $redirect = 'login';
-
-        $request = $this->getRequest();
-        if ($request->isPost()){
-            $form->setData($request->getPost());
-            if ($form->isValid()){
-                //check authentication...
-                $this->getAuthService()->getAdapter()
-                                       ->setIdentity($request->getPost('username'))
-                                       ->setCredential($request->getPost('password'));
-
-                $result = $this->getAuthService()->authenticate();
-                foreach($result->getMessages() as $message)
-                {
-                    //save message temporary into flashmessenger
-                    $this->flashmessenger()->addMessage($message);
-                }
-
-                if ($result->isValid()) {
-                    $redirect = 'success';
-                    //check if it has rememberMe :
-                    if ($request->getPost('rememberme') == 1 ) {
-                        $this->getSessionStorage()
-                             ->setRememberMe(1);
-                        //set storage again
-                        $this->getAuthService()->setStorage($this->getSessionStorage());
-                    }
-                    $this->getAuthService()->setStorage($this->getSessionStorage());
-                    $this->getAuthService()->getStorage()->write($request->getPost('username'));
-                }
-            }
-        }
-
-        return $this->redirect()->toRoute($redirect);
-    }
 
 
- /**
-  * Logout Action
-  */
- public function logoutAction() {
-  $this->getLocator()->get('User\Controller\Plugin\UserAuthentication')->clearIdentity(); //@todo - We must use PluginLoader $this->userAuthentication()!!
- }
 
 }
